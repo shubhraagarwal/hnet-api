6edd1e337d0865c1df98b21eab9080a3f8e75a22
Shubhra Agarwal <67220475+shubhraagarwal@users.noreply.github.com>
2022-04-11 11:53:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const request = require("request");
 const mongoose = require("mongoose");
 const app = express();
-
+const API_URL = "0a29-2409-4072-598-a099-fceb-890d-566c-5055.ngrok.io";
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
   "mongodb+srv://admin:admin@cluster0.wehxb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -20,56 +20,48 @@ const client = new MongoClient(
   }
 );
 
-const PORT = process.env.PORT || 3100;
+const PORT = process.env.PORT 3100;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.get("/one", async (req, res) => {
   await client.connect();
   console.log("first");
   const database = client.db("Data");
   const collection = database.collection("nodeOne");
-  request(
-    "http://127.0.0.1:5100/sensor_data1",
-    async function (error, response, body) {
-      console.log(typeof JSON.parse(response["body"]));
-      collection.deleteMany({});
-      collection.insertMany(
-        JSON.parse(response["body"]),
-        function (err, result) {
-          if (err) {
-            res.status(400).send("Error inserting matches!");
-          } else {
-            console.log(`Added a new match with id ${result.insertedId}`);
-            res.status(204).send();
-          }
-        }
-      );
-    }
-  );
+  request(`${API_URL}/sensor_data1`, async function (error, response, body) {
+    console.log(typeof JSON.parse(response["body"]));
+    collection.deleteMany({});
+    collection.insertMany(JSON.parse(response["body"]), function (err, result) {
+      if (err) {
+        res.status(400).send("Error inserting matches!");
+      } else {
+        console.log(`Added a new match with id ${result.insertedId}`);
+        res.status(204).send();
+      }
+    });
+  });
 });
 app.get("/two", async (req, res) => {
   await client.connect();
   console.log("second");
   const database = client.db("Data");
   const collection = database.collection("nodeTwo");
-  request(
-    "http://127.0.0.1:5100/sensor_data2",
-    async function (error, response, body) {
-      // console.log(JSON.parse(response["body"]));
-      // console.log(JSON.parse(response["body"]));
-      collection.deleteMany({});
-      collection.insertMany(
-        JSON.parse(response["body"]),
-        function (err, result) {
-          if (err) {
-            res.status(400).send("Error inserting matches!");
-          } else {
-            console.log(`Added a new match with id ${result.insertedId}`);
-            res.status(204).send();
-          }
-        }
-      );
-    }
-  );
+  request(`${API_URL}/sensor_data2`, async function (error, response, body) {
+    // console.log(JSON.parse(response["body"]));
+    // console.log(JSON.parse(response["body"]));
+    collection.deleteMany({});
+    collection.insertMany(JSON.parse(response["body"]), function (err, result) {
+      if (err) {
+        res.status(400).send("Error inserting matches!");
+      } else {
+        console.log(`Added a new match with id ${result.insertedId}`);
+        res.status(204).send();
+      }
+    });
+  });
 });
 
 app.get("/total_traffic", async (req, res) => {
@@ -77,26 +69,18 @@ app.get("/total_traffic", async (req, res) => {
   console.log("traffic");
   const database = client.db("Data");
   const collection = database.collection("totalTraffic");
-  request(
-    "http://127.0.0.1:5100/total_traffic",
-    async function (error, response, body) {
-      console.log(typeof JSON.parse(response["body"]));
-      collection.deleteMany({});
-      collection.insertMany(
-        JSON.parse(response["body"]),
-        function (err, result) {
-          if (err) {
-            res.status(400).send("Error inserting matches!");
-          } else {
-            console.log(`Added a new match with id ${result.insertedId}`);
-            res
-              .status(204)
-              .send(`Added a new match with id ${result.insertedId}`);
-          }
-        }
-      );
-    }
-  );
+  request(`${API_URL}/total_traffic`, async function (error, response, body) {
+    console.log(typeof JSON.parse(response["body"]));
+    collection.deleteMany({});
+    collection.insertMany(JSON.parse(response["body"]), function (err, result) {
+      if (err) {
+        res.status(400).send("Error inserting matches!");
+      } else {
+        console.log(`Added a new match with id ${result.insertedId}`);
+        res.status(204).send(`Added a new match with id ${result.insertedId}`);
+      }
+    });
+  });
 });
 
 app.get("/getnodeone", async (req, res) => {
